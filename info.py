17fe3f397a0b3be01f528ae1a954f3f89b503e77
Tygafgs <ajibcom8@gmail.com>
2025-03-21 09:51:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'MaviMon')
 API_ID = int(environ.get('API_ID', '25471015'))
 API_HASH = environ.get('API_HASH', '0ab2955ad5b1a913e220800b5fc5db36')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7755351901:AAE7DRuxs7ZHpvE3_irxUXN9uJIifOwKHq4")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
